9111d6868c61989c36152d70b2ffc6036ab550f2
miriamraide <miriamraide@gmail.com>
2024-10-01 23:24:12 -0400

    Update HomePage.jsx

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import { Carousel } from './';
 
 const HomePage = () => {
   return (
-    <div className='bg-amazonclone-background'>
+    <div className='bg-petshop-background'>
       <div className='min-w-[1000px] max-w-[1500px] m-auto'>
         <Carousel />
       </div>
