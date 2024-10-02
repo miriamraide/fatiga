30119b337e4ad40dbf1dac2b5642752808af3ea4
miriamraide <miriamraide@gmail.com>
2024-10-01 23:23:18 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {petshop: {
+        background: "#EAEDED",
+        light_blue: "#232F3A",
+        yellow: "#FEBD69",
+        DEFAULT: "#131921",
+      }},
   },
   plugins: [],
 }
