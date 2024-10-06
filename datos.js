b03147e09f44ecbb2aa95d55a2d4d01d5fe25007
miriamraide <miriamraide@gmail.com>
2024-10-06 18:24:09 -0400

    Add files via upload

---
+++ datos.js
@@ -0,0 +1,323 @@
+import React from 'react';
+import bed1 from "./public/images/bed1.jpg";
+import bed2 from "./public/images/bed2.jpg";
+import bed3 from "./public/images/bed3.jpg";
+import bed4 from "./public/images/bed4.jpg";
+import birdfeeder from "./public/images/birdfeeder.jpg";
+import birdfood from "./public/images/birdfood.jpg";
+import birdtoy from "./public/images/birdtoy.jpg";
+import birdtravel from "./public/images/birdtravel.jpg";
+import catfeeder from "./public/images/catfeeder.jpg";
+import catfoodorganic from "./public/images/catfoodorganic.jpg";
+import cattoy from "./public/images/cattoy.jpg";
+import cattravel from "./public/images/cattravel.jpg";
+import cattravel1 from "./public/images/cattravel1.jpg";
+import dogbed from "./public/images/dogbed.jpg";
+import dogbed1 from "./public/images/dogbed1.jpg";
+import dogcape from "./public/images/dogcape.jpg";
+import dogfeeder from "./public/images/dogfeeder.jpg";
+import dogfoodorganic from "./public/images/dogfoodorganic.jpg";
+import dogleashfabric from "./public/images/dogleashfabric.jpg";
+import dogleashleather from "./public/images/dogleashleather.jpg";
+import dogtoychewballs from "./public/images/dogtoychewballs.jpg";
+import dogtravel from "./public/images/dogtravel.jpg";
+import dogtravelride from "./public/images/dogtravelride.jpg";
+import dogtravelsuitcase from "./public/images/dogtravelsuitcase.jpg";
+import dogvestwool from "./public/images/dogvestwool.jpg";
+import fishbowl from "./public/images/fish-bowl.jpg";
+import fishbowlambiencedecorationpinkcorals from "./public/images/fishbowlambiencedecorationpinkcorals.jpg";
+import fishfoodorganic from "./public/images/fishfoodorganic.jpg";
+import fishtank from "./public/images/fishtank.jpg";
+import hamsterfoodorganic from "./public/images/hamsterfoodorganic.jpg";
+import hamstertoymaze from "./public/images/hamstertoymaze.jpg";
+import hamsterwheel from "./public/images/hamsterwheel.jpg";
+import petcar from "./public/images/petcar.jpg";
+import pettag from "./public/images/pettag.jpg";
+import pettag1 from "./public/images/pettag1.jpg";
+import pettag3 from "./public/images/pettag3.jpg";
+import recycledcatpooper from "./public/images/recycledcatpooper .jpg";
+
+export const data = [
+  {
+    key: 1,
+    name: "Cama/mochila para mascotas pequeñas",
+    description: "Tejida por abuelas del Noreste Argentino, esta original camita es super liviana y flexible, lista para llevar a todas partes",
+    image: bed2,
+    precio: 25000
+  },
+  {
+    key: 2,
+    name: "Cama moderna, ideal para mascotas pequeñas",
+    description: "Super contemporánea, y muy original, ideal para quienes buscan un estilo de decoración moderno",
+    image: bed1,
+    precio: 55000
+  },
+
+  {
+    key: 3,
+    name: "Cama de fibras largas y tupidas, moderna, ideal para mascotas pequeñas o medianas",
+    description: "Estilo muy confortable para mascotas, y muy actual a la hora de combinar con la decoracion del hogar",
+    image: bed3,
+    precio: 35000
+  },
+  {
+    key: 4,
+    name: "Sillon / Cama, moderno, ideal para mascotas pequeñas o medianas",
+    description: "Este estilo no desilusiona, es como estar recostado en una montaña de almohadones.",
+    image: bed4,
+    precio: 45000
+  },
+
+  {
+    key: 5,
+    name: "Alimentador con néctar para pájaros",
+    description: "Fácil de instalar, es un atractivo para ver alimentarse desde tu ventana, aves de distintos tipos y colores. Un sueño!.",
+    image: birdfeeder,
+    precio: 47000
+  },
+  {
+    key: 6,
+    name: "Alimento órganico para pájaros",
+    description: "Cosechado en las mejores regiones del Noreste Argentino, estas semillas orgánicas son espectaculares. Se venden por kilo ",
+    image: birdfood,
+    precio: 12000
+  },
+  {
+    key: 7,
+    name: "Soga trenzada para aves",
+    description: "Totalmente realizada a mano por aborígenes del Norte Argentino ",
+    image: birdtoy,
+    precio: 28000
+  },
+  {
+    key: 8,
+    name: "Jaula viajera para el transporte de aves domésticas",
+    description: "Realizada 100% con productos reciclados, por una compañía local del Gran Bs. As ",
+    image: birdtravel,
+    precio: 37000
+  },
+
+  {
+    key: 9,
+    name: "Plato sopero para servir alimentos",
+    description: "Realizado a mano con elementos totalmente reciclables",
+    image: catfeeder,
+    precio: 22000
+  },
+
+  {
+    key: 10,
+    name: "Alimento para gatos orgánico",
+    description: "Ingrediente de primera calidad, sin agregados químicos. Se vende por kilo",
+    image: catfoodorganic,
+    precio: 15000
+  },
+  {
+    key: 11,
+    name: "Pompones y Plumerillos Colgantes para mascotas",
+    description: "Fácil de instalar, y super atractivo para todas las mascotas, especialmente los gatos",
+    image: cattoy,
+    precio: 11000
+  },
+  {
+    key: 12,
+    name: "Mochila de cuero ecológico para transportar mascotas",
+    description: "Totalmente realizada a mano por aborígenes del Norte Argentino, con elementos reciclables ",
+    image: cattravel,
+    precio: 68000
+  },
+  {
+    key: 13,
+    name: "Valija pequeña para el transporte de mascotas",
+    description: "Totalmente realizada a mano por aborígenes del Norte Argentino, con elementos reciclables ",
+    image: cattravel1,
+    precio: 57000
+  },
+
+  {
+    key: 14,
+    name: "Cama contemporánea para perros de tamaño mediano y grande, con luz led incorporada",
+    description: "Estilo muy confortable para mascotas, y muy actual a la hora de combinar con la decoracion del hogar",
+    image: dogbed,
+    precio: 35000
+  },
+  {
+    key: 15,
+    name: "Cama cucheta estilo lujoso para perros de tamaño mediano y grande",
+    description: "Estilo muy confortable para mascotas, de un diseño ideal para quienes aman el estilo vintage",
+    image: dogbed1,
+    precio: 95000
+  },
+  {
+    key: 16,
+    name: "Capa estilo british gentleman, en color red ",
+    description: "Totalmente realizada a mano por grupos de rescatista de animales, del Noreste Argentino",
+    image: dogcape,
+    precio: 28000
+  },
+  {
+    key: 17,
+    name: "Alimentador automático para mascotas",
+    description: "Eficiencia, control, independencia, hacen de este Feeder, el mejor de todos",
+    image: dogfeeder,
+    precio: 115000
+  },
+
+  {
+    key: 18,
+    name: "Alimento orgánico para perros",
+    description: "Ingrediente de primera calidad, sin agregados químicos. Se vende por kilo",
+    image: dogfoodorganic,
+    precio: 15000
+  },
+  {
+    key: 19,
+    name: "Correa para perros realizada en ecocuero",
+    description: "Realizadas a mano con elementos totalmente reciclables",
+    image: dogleashleather,
+    precio: 45000
+  },
+  {
+    key: 20,
+    name: "Correa para perros realizada en soga resistente",
+    description: "Realizadas a mano con elementos totalmente reciclables",
+    image: dogleashfabric,
+    precio: 29000
+  },
+  {
+    key: 21,
+    name: "Bolas de morder para perros",
+    description: "Realizadas con elementos totalmente reciclables",
+    image: dogtoychewballs,
+    precio: 15000
+  },
+
+  {
+    key: 22,
+    name: "Mochila para transportar mascotas pequeñas",
+    description: "Totalmente realizada a mano por aborígenes del Norte Argentino, con elementos reciclables ",
+    image: dogtravel,
+    precio: 24000
+  },
+
+  {
+    key: 23,
+    name: "Jaula para transportar mascotas pequeñas",
+    description: "Realizadas con elementos totalmente reciclables",
+    image: dogtravelride,
+    precio: 35000
+  },
+
+  {
+    key: 24,
+    name: "Valija carrito con ruedas para transportar mascotas pequeñas",
+    description: "Eficiencia, calidad, confortable, hacen de este carrito, el mejor de todos",
+    image: dogtravelsuitcase,
+    precio: 87000
+  },
+
+  {
+    key: 25,
+    name: "Chaleco de lana para usar en invierno",
+    description: "Tejida por abuelas del Noreste Argentino, este original chaleco mantiene a todas las mascotas muy calentitas",
+    image: dogvestwool,
+    precio: 19000
+  },
+  {
+    key: 26,
+    name: "Pecera de vidrio de tamaño ideal para espacios pequeños",
+    description: "Realizadas con elementos totalmente reciclables",
+    image: fishbowl,
+    precio: 25000
+  },
+
+  {
+    key: 27,
+    name: "Pecera decorativa con Corales Rosados para usar como ambientación",
+    description: "Realizadas con elementos totalmente reciclables",
+    image: fishbowlambiencedecorationpinkcorals,
+    precio: 27000
+  },
+
+  {
+    key: 28,
+    name: "Alimento orgánico para peces",
+    description: "Realizados con ingredientes de alta calidad. Se vende desde 100 gramos",
+    image: fishfoodorganic,
+    precio: 5000
+  },
+
+  {
+    key: 29,
+    name: "Pecera iluminada ultra moderna, con todos los elementos de mantenimiento del agua incluidos.",
+    description: "Eficiencia, calidad, estilo, hacen de esta pecera, la mejor de todas",
+    image: fishtank,
+    precio: 95000
+  },
+
+
+  {
+    key: 30,
+    name: "Alimento orgánico para hamsters",
+    description: "Realizados con ingredientes de alta calidad. Se vende desde 100 gramos",
+    image: hamsterfoodorganic,
+    precio: 5000
+  },
+
+  {
+    key: 31,
+    name: "Juego de laberinto para hamsters",
+    description: "Realizadas con elementos totalmente reciclables",
+    image: hamstertoymaze,
+    precio: 35000
+  },
+
+  {
+    key: 32,
+    name: "Rueda de ejercicios para hamsters",
+    description: "Realizadas con elementos totalmente reciclables",
+    image: hamsterwheel,
+    precio: 45000
+  },
+
+  {
+    key: 33,
+    name: "Asiento de mascotas para autos",
+    description: "Realizadas a mano, con elementos totalmente reciclables",
+    image: petcar,
+    precio: 39000
+  },
+
+  {
+    key: 34,
+    name: "Tags de identificacion para mascotas en plástico reciclado",
+    description: "Realizadas a mano, con elementos totalmente reciclables",
+    image: pettag,
+    precio: 18000
+  },
+
+  {
+    key: 35,
+    name: "Tags de identificacion para mascotas en ecocuero reciclado",
+    description: "Realizadas a mano, con elementos totalmente reciclables",
+    image: pettag1,
+    precio: 25000
+  },
+
+  {
+    key: 36,
+    name: "Tags de identificacion para mascotas en acero quirúrgico pintado",
+    description: "Realizadas a mano, con elementos totalmente reciclables",
+    image: pettag3,
+    precio: 28000
+  },
+
+  {
+    key: 37,
+    name: "Caja de evacuación para gatos, reusable",
+    description: "Realizadas con elementos totalmente reciclables",
+    image: recycledcatpooper,
+    precio: 37000
+  },
+
+]
